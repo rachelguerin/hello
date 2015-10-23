a3404af6726a73abf9dad90a4cff88c6499fd06b
Rachel <rachel@tyba.com>
2015-10-23 11:34:39 +0200

    First commit

---
+++ hello/mofo.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func mofo() {
+	fmt.Printf("hello, world\n")
+	fmt.Printf("yo mofo")
+}
---
+++ hello/showtime.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func showtime() {
+	fmt.Println("Welcome to the playground!")
+
+	fmt.Println("The time is", time.Now())
+}
